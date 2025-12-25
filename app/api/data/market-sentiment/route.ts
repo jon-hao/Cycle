d4bcc51f58c7439fd15d14d762b83618117e93fc
Albatrosses <capitalism_4418@163.com>
2025-12-25 02:06:26 +0000

    Update

--- app/api/data/market-sentiment/route.ts
+++ app/api/data/market-sentiment/route.ts
@@ -1,3 +1,28 @@
-export async function GET(request: Request) {
-  return Response.json({ message: 'Hello World' })
+import getConfig from "@/common/configs";
+import { Domain, Platform } from "@/common/constants/serviceEnum";
+import { fetchData } from "@/common/helpers/requestHelper";
+
+const config = getConfig(Domain.Data, Platform.CNN);
+
+export const GET = async (request: Request) => {
+  try {
+      const { marketSentiment: { url } } = config;
+      const result = await fetchData(url, { isEmulateBrowser: true });
+      const currentFearGreed = {
+        score: result['fear_and_greed'].score,
+        rating: result['fear_and_greed'].rating
+      };
+      const fearGreedTrends = result['fear_and_greed_historical'].data.map(({ x, y, rating }) => ({
+        timestamp: x,
+        score: y,
+        rating
+      }));
+      const data = {
+        currentFearGreed,
+        fearGreedTrends
+      }
+      return Response.json(data);
+    } catch(e) {
+      return null
+    }
 }
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,7 @@
-const Home = () => {
+export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      test
+      The page not found
     </div>
   );
 }
-
-export default Home
